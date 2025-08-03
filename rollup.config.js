e560d11497677206b6d87e0d8603c6ee44b73a22
Thomas <152863307+Thomas-TF1@users.noreply.github.com>
2025-08-03 23:28:20 +0200

    setup minimal ts configs + rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+      exports: "named",
+    },
+  ],
+  plugins: [typescript({ useTsconfigDeclarationDir: true })],
+  external: ["react", "react-native"],
+};
